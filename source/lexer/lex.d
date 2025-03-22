492f69617856a222155b53e5a6d27c94a0441583
Albert <albertbear94@gmail.com>
2025-03-21 22:00:41 -0500

    added token enum

--- source/lexer/lex.d
+++ source/lexer/lex.d
@@ -2,11 +2,25 @@ module lexer.lex;
 
 import std.stdio;
 
-class Lexer {
+enum TOK {
+  OPEN_PAREN,
+  CLOSE_PAREN,
+  NUMBER,
+  IDENT,
+  EQUAL,
+  LESS_EQ,
+  LESS,
+  GREATER,
+  GREATER_EQ,
+}
+
+struct Lexer {
+  string _input;
 
   this(string input) { _input = input; }
+
   public void run() {
     writeln("Tokenizing...");
   }
-  private string _input;
+
 }
